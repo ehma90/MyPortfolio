a01baafacd5d07bd15bce7c5c0272015c2be8d09
Emmanuel Essien <ehmaessien@gmail.com>
2023-10-25 15:12:43 +0100

    Add Google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import ReactGA from 'react-ga';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  // Initialize Google Analytics with Tracking ID
+  ReactGA.initialize(process.env.GOOGLE_TRACKING_ID as string);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+  }, [router]);
   return <Component {...pageProps} />
 }
