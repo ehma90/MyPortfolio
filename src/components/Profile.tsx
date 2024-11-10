c3d34106b7b05dddea0b4d610a7fb43894f62775
Emmanuel Essien <76791460+ehma90@users.noreply.github.com>
2024-11-10 16:27:37 +0100

    Update Profile.tsx

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -14,7 +14,7 @@ function Profile() {
     >
       <ul className=" w-full space-y-2 md:space-y-4 mb-5">
         <li className="leading-7 text-base text-white/70">
-          Hello 👋🏽, I&apos;m Emmanuel,a professional Full Stack Developer with 4 years of experience in both learning and working in the field. I specialise in delivering interactive, responsive, and user-friendly web and mobile applications that resonate with users globally. My core strengths lie in full-stack development, where I leverage the latest front-end and back-end technologies to create scalable, efficient, and high-performing applications. I’m passionate about building complete solutions, from database architecture to seamless user interfaces, ensuring a smooth and impactful experience.
+          Hello 👋🏽, I&apos;m Emmanuel,a professional Full Stack Developer with 3 years of experience in both learning and working in the field. I specialise in delivering interactive, responsive, and user-friendly web and mobile applications that resonate with users globally. My core strengths lie in full-stack development, where I leverage the latest front-end and back-end technologies to create scalable, efficient, and high-performing applications. I’m passionate about building complete solutions, from database architecture to seamless user interfaces, ensuring a smooth and impactful experience.
           I have considerable expertise in backend development, particularly in building and testing APIs, as well as managing data storage and retrieval from databases. 
         </li>
         <li className="leading-7 text-base text-white/70">
